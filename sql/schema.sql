208ee6a1af00511e9676ff3a20a9f583580910dd
Knitter <knitter.is@gmail.com>
2023-08-24 20:28:25 +0100

    Adds initial schema and demo data
    - Initial schema based on older codices project; will need to be changed for desktop version;
    - Basic exemple data to allow initial screens to be tested on read first implementation;

---
+++ sql/schema.sql
@@ -0,0 +1,108 @@
+-- DROP TABLE item;
+-- DROP TABLE publisher;
+-- DROP TABLE collection;
+-- DROP TABLE series;
+-- DROP TABLE account;
+
+
+CREATE TABLE account
+(
+    id         INTEGER PRIMARY KEY,
+    username   TEXT    NOT NULL UNIQUE,
+    email      TEXT    NOT NULL,
+    name       TEXT    NOT NULL,
+    active     INTEGER NOT NULL,
+    password   TEXT    NOT NULL,
+    created_at INTEGER NOT NULL,
+    updated_at INTEGER NOT NULL,
+    resetToken TEXT    NULL UNIQUE,
+    authKey    TEXT
+);
+
+CREATE TABLE publisher
+(
+    id        INTEGER PRIMARY KEY,
+    name      TEXT    NOT NULL,
+    ownedById INTEGER NOT NULL,
+    summary   TEXT,
+    website   TEXT,
+    logo      TEXT,
+    FOREIGN KEY (ownedById) REFERENCES account (id)
+);
+
+CREATE TABLE series
+(
+    id         INTEGER PRIMARY KEY,
+    name       TEXT    NOT NULL,
+    ownedById  INTEGER NOT NULL,
+    completed   INTEGER NOT NULL,
+    bookCount  INTEGER,
+    ownedCount INTEGER,
+    FOREIGN KEY (ownedById) REFERENCES account (id)
+);
+
+-- A collection of books either created by the user or by a publisher/seller (e.g. HumbleBundle)
+CREATE TABLE collection
+(
+    id          INTEGER PRIMARY KEY,
+    name        TEXT    NOT NULL,
+    ownedById   INTEGER NOT NULL,
+    publishDate TEXT,
+    publishYear INTEGER,
+    FOREIGN KEY (ownedById) REFERENCES account (id)
+);
+
+CREATE TABLE author
+(
+    id        INTEGER PRIMARY KEY,
+    name      TEXT    NOT NULL,
+    ownedById INTEGER NOT NULL,
+    surname   TEXT,
+    biography TEXT,
+    website   TEXT,
+    photo     TEXT,
+    FOREIGN KEY (ownedById) REFERENCES account (id)
+);
+
+-- A paper book, ebook or audio book
+CREATE TABLE item
+(
+    id            INTEGER PRIMARY KEY,
+    title         TEXT    NOT NULL,
+    ownedById     INTEGER NOT NULL,
+    type          TEXT    NOT NULL, -- ebook, audio, paper
+    translated    INTEGER NOT NULL,
+    favorite      INTEGER NOT NULL,
+    read          INTEGER NOT NULL,
+    copies        INTEGER NOT NULL,
+    subtitle      TEXT,
+    originalTitle TEXT,
+    plot          TEXT,
+    isbn          TEXT,
+    format        TEXT,
+    pageCount     INTEGER,
+    publishDate   TEXT,
+    publishYear   INTEGER,
+    addedOn       TEXT,
+    language      TEXT,
+    edition       TEXT,
+    rating        REAL,
+    ownRating     REAL,
+    url           TEXT,
+    review        TEXT,
+    cover         TEXT,
+    filename      TEXT,
+    narrator      TEXT,
+    boughtFrom    TEXT,
+    duration      INTEGER,          -- minutes
+    orderInSeries INTEGER,
+    publisherId   INTEGER,
+    seriesId      INTEGER,
+    collectionId  INTEGER,
+    duplicatesId  INTEGER,
+    FOREIGN KEY (ownedById) REFERENCES account (id),
+    FOREIGN KEY (publisherId) REFERENCES publisher (id),
+    FOREIGN KEY (seriesId) REFERENCES series (id),
+    FOREIGN KEY (collectionId) REFERENCES collection (id),
+    FOREIGN KEY (duplicatesId) REFERENCES item (id)
+);
