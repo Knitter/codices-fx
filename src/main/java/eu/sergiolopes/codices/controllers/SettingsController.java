b270206dd4f463db08c5900ca70897b85b233bbb
Knitter <knitter.is@gmail.com>
2023-08-26 02:18:52 +0100

    Adds settings controller placeholder

---
+++ src/main/java/eu/sergiolopes/codices/controllers/SettingsController.java
@@ -0,0 +1,65 @@
+/**
+ * MIT License
+ * <p>
+ * Copyright (c) 2023 Sérgio Lopes
+ * https:www.sergiolopes.eu, knitter.is@gmail.com
+ * <p>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * <p>
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * <p>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package eu.sergiolopes.codices.controllers;
+
+import eu.sergiolopes.codices.models.Author;
+import eu.sergiolopes.codices.repositories.AuthorRepository;
+import eu.sergiolopes.codices.view.ViewManager;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class SettingsController extends Controller implements Initializable {
+
+    public SettingsController(ViewManager vm, String fxml) {
+        super(vm, fxml);
+    }
+
+    @Override
+    public String getTitle() {
+        return "Codices Settings";
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        //TODO: ...
+    }
+
+    public void saveChanges() {
+        //TODO: ...
+    }
+
+    public void closeWindow() {
+        getManager().closeCurrentStage();
+    }
+
+}
